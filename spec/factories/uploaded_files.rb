a2d9ab341c5494ae3e97a1095fa5f0226877e668
ovaldez-at-022258092259 <Odvesims@gmail.com>
2021-04-19 06:42:13 -0400

    Setting redis and sidekiq for heroku

--- spec/factories/uploaded_files.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :uploaded_file do
-    title { "MyString" }
-    state { "MyString" }
-  end
-end
--- spec/workers/process_file_worker_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-RSpec.describe ProcessFileWorker, type: :worker do
-  describe "worker" do
-    it "runs the worker" do
-      expect(ProcessFileWorker.perform_async(129, ['name', 'date_of_birth', 'phone', 'address', 'credit_card', 'email'], )).to eq(true)
-    end
-  end
-end
